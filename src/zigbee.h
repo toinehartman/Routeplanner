b3bf1e2a6164c3d372c986920fb12c4038d91eaf
Toine Hartman <tjbhartman@gmail.com>
2014-04-01 14:41:10 +0200

    Rebased a week long, fixed.

---
+++ src/zigbee.h
@@ -0,0 +1,14 @@
+#ifndef ZIGBEE_H
+#define ZIGBEE_H
+
+#include <Windows.h>
+
+#define COMPORT "COM5"
+#define BAUDRATE CBR_9600
+
+void initSio(HANDLE hSerial);
+int readByte(HANDLE hSerial, char *buffRead);
+int writeByte(HANDLE hSerial, char *buffWrite);
+int mains();
+
+#endif /* ZIGBEE_H */
