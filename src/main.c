5d73654ca7547ad9d2042f6f08290e7ad112f9ff
Toine Hartman <tjbhartman@gmail.com>
2014-05-28 03:56:44 +0200

    Added ZigBee for Windows and Code::Blocks project.

--- src/main.c
+++ src/main.c
@@ -9,69 +9,113 @@
 #include "main.h"
 #include "data.h"
 #include "functions.h"
+#include "zigbee.h"
 
-int main(int argc, char* argv[]) {
-	int i, j, *cp, *new_cp, cp_num, a;
-	
-	cp_num = argc - 1;
-	cp = (int*) calloc(cp_num, sizeof(int));
+int main(int argc, char* argv[])
+{
+    int i, j, *cp, cp_num, a;
 
-	if (cp_num >= 1) { /* als checkpoints in argument zijn meegegeven, deze gebruiken */
-		for (i = 0; i < cp_num; i++) {
-			a = atoi(argv[i + 1]);
+    /* ZigBee initialisations */
+    HANDLE hSerial;
 
-			if (a >= 1 && a <= 12)
-				cp[i] = atoi(argv[i + 1]);
-			else {
-				printf("%d is not a number of an existing checkpoint!!\n", a);
-				exit(EXIT_FAILURE);
-			}
-		}
+    char byteBuffer[BUFSIZ+1];
 
-		if (cp[0] != START_CP) {
-			cp_num++;
-			if ((new_cp = (int*) realloc(cp, (cp_num) * sizeof(int))) == NULL) {
-				free(cp);
-				printf("ERROR: Unable to allocate enough memory for all destinations!\n");
-				exit(EXIT_FAILURE);
-			}
-			cp = new_cp;
-			free (new_cp);
-			
-			for (i = cp_num - 1; i > 0; i--)
-				cp[i] = cp[i - 1];
-			cp[0] = START_CP;
-		}
+    /*
+        //----------------------------------------------------------
+        // Open COMPORT for reading and writing
+        //----------------------------------------------------------
+        */
+    hSerial = CreateFile(COMPORT,
+                         GENERIC_READ | GENERIC_WRITE,
+                         0,
+                         0,
+                         OPEN_EXISTING,
+                         FILE_ATTRIBUTE_NORMAL,
+                         0
+                        );
 
-		printf("-----------------------------------\n");
-		printf("Number of target checkpoints: %d\n", cp_num - 1);
+    if(hSerial == INVALID_HANDLE_VALUE)
+    {
+        if(GetLastError()== ERROR_FILE_NOT_FOUND)
+        {
+            /* //serial port does not exist. Inform user.*/
+            printf(" serial port does not exist \n");
+        }
+        /*    //some other error occurred. Inform user.*/
+        printf(" some other error occured. Inform user.\n");
+    }
+    /*
+        //----------------------------------------------------------
+        // Initialize the parameters of the COM port
+        //----------------------------------------------------------
+    */
+    initSio(hSerial);
 
-		/* print checkpoints to go to */
-		printf("Start at %d, go to: ", cp[0]);
-		for (i = 1; i < cp_num; i++)
-			printf("%d ", cp[i]);
-		printf("\n-----------------------------------\n\n");
-	} 
-	else { /* anders ERROR */
-		printf("At least 1 argument required!\n");
-		exit(EXIT_FAILURE);
-	}
+    cp_num = argc - 1;
+    cp = (int*) calloc(cp_num, sizeof(int));
 
-	printf("\n");
+    if (cp_num >= 1)   /* als checkpoints in argument zijn meegegeven, deze gebruiken */
+    {
+        for (i = 0; i < cp_num; i++)
+        {
+            a = atoi(argv[i + 1]);
 
-	init_field();
-	link_nodes();
-	print_field();
+            if (a >= 1 && a <= 12)
+                cp[i] = a;
+            else
+            {
+                printf("%d is not a number of an existing checkpoint!!\n", a);
+                exit(EXIT_FAILURE);
+            }
+        }
 
-	short_sort(cp, cp_num);
+        if (cp[0] != START_CP)
+        {
+            cp_num++;
+            if ((cp = (int*) realloc(cp, (cp_num) * sizeof(int))) == NULL)
+            {
+                printf("ERROR: Unable to allocate enough memory for all destinations!\n");
+                exit(EXIT_FAILURE);
+            }
 
-	printf("-------\nSorted!\n");
-	for (j = 0; j < cp_num; j++) printf("%d ", cp[j]);
-	printf("\n-------\n\n");
+            for (i = cp_num - 1; i > 0; i--)
+                cp[i] = cp[i - 1];
+            cp[0] = START_CP;
+        }
 
-	if (read_mines() == 1) printf("No mine file!\n");
+        printf("-----------------------------------\n");
+        printf("Number of target checkpoints: %d\n", cp_num - 1);
 
-	route_sequence(cp, cp_num);
+        /* print checkpoints to go to */
+        printf("Start at %d, go to: ", cp[0]);
+        for (i = 1; i < cp_num; i++)
+            printf("%d ", cp[i]);
+        printf("\n-----------------------------------\n\n");
+    }
+    else   /* anders ERROR */
+    {
+        printf("At least 1 argument required!\n");
+        exit(EXIT_FAILURE);
+    }
 
-	return 0;
+    printf("\n");
+
+    init_field();
+    link_nodes();
+    print_field();
+
+    short_sort(cp, cp_num);
+
+    printf("-------\nSorted!\n");
+    for (j = 0; j < cp_num; j++) printf("%d ", cp[j]);
+    printf("\n-------\n\n");
+
+    if (read_mines() == 1) printf("No mine file!\n");
+
+    route_sequence(hSerial, byteBuffer, cp, cp_num);
+    free(cp);
+
+    CloseHandle(hSerial);
+
+    return 0;
 }
---
+++ src/zigbee.h
@@ -0,0 +1,14 @@
+#ifndef ZIGBEE_H
+#define ZIGBEE_H
+
+#include <Windows.h>
+
+#define COMPORT "COM4"
+#define BAUDRATE 9600
+
+void initSio(HANDLE hSerial);
+int readByte(HANDLE hSerial, char *buffRead);
+int writeByte(HANDLE hSerial, char *buffWrite);
+int mains();
+
+#endif /* ZIGBEE_H */
